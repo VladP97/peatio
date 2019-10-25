ff2fc248e3f2ace9f16b6842c40c5fad0103e252
VladP97 <vlad.potapov1997@gmail.com>
2019-10-25 15:11:51 +0300

    implemented actioncable for orders

--- app/api/v2/market/orders.rb
+++ app/api/v2/market/orders.rb
@@ -72,6 +72,8 @@ module API
             error!({ errors: ['market.order.market_order_price'] }, 422)
           end
           order = create_order(params)
+          order_market = Market.find(params[:market])
+          MarketsChannel.broadcast_to order_market, order: order
           present order, with: API::V2::Entities::Order
         end
 
