0b39dd38c7eda83275400c9d7204f16253855f28
VladP97 <vlad.potapov1997@gmail.com>
2019-11-13 12:42:55 +0300

    fix: changed order notification object

--- app/api/v2/order_helpers.rb
+++ app/api/v2/order_helpers.rb
@@ -46,7 +46,7 @@ module API
 
       def new_order_notify(market, order)
         order_market = ::Market.find(market)
-        MarketsChannel.broadcast_to order_market, price: order.price, volume: order.volume, type: order.type
+        MarketsChannel.broadcast_to order_market, order
       end
 
       def submit_order(order)
