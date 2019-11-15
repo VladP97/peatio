bf9576813f9d4cfcbdb846f273d68cdf0fa60c2f
VladP97 <vlad.potapov1997@gmail.com>
2019-11-15 13:38:38 +0300

    fix

--- app/trading/matching/trades_notifications.rb
+++ app/trading/matching/trades_notifications.rb
@@ -3,20 +3,22 @@ module Matching
     def notify(trade, market)
       header_service = HeaderService.new(market)
       trade_with_min_price, trade_with_max_price = header_service.trades_with_min_max_price
+      volume = header_service.sum_of_daily_trades
+      diff = header_service.price_diff(trade)
       TradesChannel.broadcast_to market, \
         trade: trade, \
-        header: get_header_info(trade_with_min_price, trade_with_max_price, header_service), \
+        header: get_header_info(trade_with_min_price, trade_with_max_price, volume, diff), \
         trade_orders: OrderBook.new(OrderBid.find(trade.maker_order_id), OrderAsk.find(trade.taker_order_id))
     end
 
     private
 
-    def get_header_info(trade_with_min_price, trade_with_max_price, header_service)
+    def get_header_info(trade_with_min_price, trade_with_max_price, volume, diff)
       {
         min_price: trade_with_min_price,
         max_price: trade_with_max_price,
-        volume: header_service.sum_of_daily_trades,
-        diff: header_service.price_diff(trade)
+        volume: volume,
+        diff: diff
       }
     end
   end
