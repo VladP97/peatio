7f0faeb5582e3ed4b944ab88fbf7d01db4614dd5
VladP97 <vlad.potapov1997@gmail.com>
2019-11-15 13:31:05 +0300

    changed trades notifications response format

--- app/trading/matching/trades_notifications.rb
+++ app/trading/matching/trades_notifications.rb
@@ -5,10 +5,19 @@ module Matching
       trade_with_min_price, trade_with_max_price = header_service.trades_with_min_max_price
       TradesChannel.broadcast_to market, \
         trade: trade, \
-        min_price: trade_with_min_price, \
-        max_price: trade_with_max_price, \
-        volume: header_service.sum_of_daily_trades, \
+        header: get_header_info(trade_with_min_price, trade_with_max_price, header_service), \
+        trade_orders: OrderBook.new(OrderBid.find(trade.maker_order_id), OrderAsk.find(trade.taker_order_id))
+    end
+
+    private
+
+    def get_header_info(trade_with_min_price, trade_with_max_price, header_service)
+      {
+        min_price: trade_with_min_price,
+        max_price: trade_with_max_price,
+        volume: header_service.sum_of_daily_trades,
         diff: header_service.price_diff(trade)
+      }
     end
   end
 end
