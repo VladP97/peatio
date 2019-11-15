73b5354da1b05c992369e16c5e90d4508bd46d29
tsygynder <vlad.potapov1997@gmail.com>
2019-11-15 09:33:37 +0000

    fix

--- app/trading/matching/trades_notifications.rb
+++ app/trading/matching/trades_notifications.rb
@@ -5,8 +5,8 @@ module Matching
       trade_with_min_price, trade_with_max_price = header_service.trades_with_min_max_price
       TradesChannel.broadcast_to market, \
         trade: trade, \
-        min_price: trade_with_min_price.price, \
-        max_price: trade_with_max_price.price, \
+        min_price: trade_with_min_price, \
+        max_price: trade_with_max_price, \
         volume: header_service.sum_of_daily_trades, \
         diff: header_service.price_diff(trade)
     end
