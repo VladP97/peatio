a7d05625afef65d6f94caf86dde47989ba7f7f5c
VladP97 <vlad.potapov1997@gmail.com>
2019-11-20 13:12:49 +0300

    changed trades notifications orders object generation

--- app/trading/matching/trades_notifications.rb
+++ app/trading/matching/trades_notifications.rb
@@ -1,5 +1,11 @@
 module Matching
   class TradesNotifications
+
+    ORDER_TYPES = {
+        'OrderBid' => :order_bid,
+        'OrderAsk' => :order_ask
+    }
+
     def notify(trade, market)
       header_service = HeaderService.new(market)
       trade_with_min_price, trade_with_max_price = header_service.trades_with_min_max_price
@@ -8,7 +14,7 @@ module Matching
       TradesChannel.broadcast_to market, \
         trade: trade, \
         header: get_header_info(trade_with_min_price, trade_with_max_price, volume, diff), \
-        trade_orders: get_trade_orders(OrderBid.find(trade.maker_order_id), OrderAsk.find(trade.taker_order_id))
+        trade_orders: get_trade_orders([Order.find(trade.taker_order_id), Order.find(trade.maker_order_id)])
     end
 
     private
@@ -22,8 +28,8 @@ module Matching
       }
     end
 
-    def get_trade_orders(order_bid, order_ask)
-      { order_bid: order_bid, order_ask: order_ask }
+    def get_trade_orders(orders)
+      Hash[orders.map { |order| [ORDER_TYPES[order.type], order] }]
     end
   end
 end
